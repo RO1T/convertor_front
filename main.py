ba51472267b45456ad663de677a2d3ede0f52cf0
RO1T <104246248+RO1T@users.noreply.github.com>
2022-11-03 20:58:25 +0500

    main commit

--- main.py
+++ main.py
@@ -9,52 +9,41 @@ from PyQt5.QtGui import QFont, QFontDatabase, QIcon
 class WindowGetResult(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Window22222")
+        self.setWindowTitle("WindowGetResult")
 
 
 # Окно с основной работой программы (конвертирование)
 class WindowWork(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Window22222")
+        self.setWindowTitle("WindowWork")
 
 
 # Окно с загрузкой и изменением исходных файлов
 class WindowDownload(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('app_1.ui', self)
+        uic.loadUi('app_2.ui', self)
         QFontDatabase.addApplicationFont("font/Gilroy-Regular.ttf")
         font = QFont('Gilroy')
         self.setWindowIcon(QIcon('logo.png'))
-        self.title = "WindowDownload"
-        self.label_down = self.findChild(QLabel, 'label_down')
-        self.label_up = self.findChild(QLabel, 'label_up')
-        labels = [self.label_up, self.label_down]
-        for label in labels:
-            label.setFont(font)
+        self.title = "Конвертор"
+        self.return_btn.setFont(font)
+        self.return_btn.clicked.connect(self.window_main_work)
 
-        self.exel_exel_btn = self.findChild(QPushButton, 'exel_exel_btn')
-        self.exel_word_btn = self.findChild(QPushButton, 'exel_word_btn')
-        self.exel_json_btn = self.findChild(QPushButton, 'exel_json_btn')
-        buttons = [self.exel_exel_btn, self.exel_word_btn, self.exel_json_btn]
-        for btn in buttons:
-            btn.setFont(font)
-
-        # btn.clicked может подчеркиваться, но это нормально,
-        # PyCharm видимо не видит что это btn , так как они инициализируются во время runtime
-        # (20-22 lines of code)
-        # ((PyQt5-stubs убирают подчеркивания))
-        self.exel_exel_btn.clicked.connect(self.window_download)
         self.main_window()
+
+    @staticmethod
+    def input_files_in_programm():
+        print('files had been inputed')
 
     def main_window(self):
         self.setWindowTitle(self.title)
         self.show()
 
-    def window_download(self):
-        self.w = WindowChoice()
-        self.w.show()
+    def window_main_work(self):
+        self.win_main = WindowChoice()
+        self.win_main.show()
         self.hide()
 
 
@@ -67,26 +56,13 @@ class WindowChoice(QMainWindow):
         font = QFont('Gilroy')
         self.setWindowIcon(QIcon('logo.png'))
         self.title = "Конвертор"
-        self.label_down = self.findChild(QLabel, 'label_down')
-        self.label_up = self.findChild(QLabel, 'label_up')
         labels = [self.label_up, self.label_down]
         for label in labels:
             label.setFont(font)
-
-        self.exel_exel_btn = self.findChild(QPushButton, 'exel_exel_btn')
-        self.exel_word_btn = self.findChild(QPushButton, 'exel_word_btn')
-        self.exel_json_btn = self.findChild(QPushButton, 'exel_json_btn')
         buttons = [self.exel_exel_btn, self.exel_word_btn, self.exel_json_btn]
         for btn in buttons:
             btn.setFont(font)
-
-        # btn.clicked может подчеркиваться, но это нормально,
-        # PyCharm видимо не видит что это btn , так как они инициализируются во время runtime
-        # (20-22 lines of code)
-        # ((PyQt5-stubs убирают подчеркивания))
-        self.exel_exel_btn.clicked.connect(self.window_download)
-        self.exel_word_btn.clicked.connect(self.exel_word_func)
-        self.exel_json_btn.clicked.connect(self.exel_json_func)
+            btn.clicked.connect(self.window_download)
         self.main_window()
 
     @staticmethod
@@ -106,8 +82,8 @@ class WindowChoice(QMainWindow):
         self.show()
 
     def window_download(self):
-        self.w = WindowDownload()
-        self.w.show()
+        self.win_d = WindowDownload()
+        self.win_d.show()
         self.hide()
 
 
